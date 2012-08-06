c3537f70d84fc4896bb940f26db4e050e2c62b98
Andrew Cobby <andrew.cobby@theglobalmail.org>
2012-08-06 16:39:53 +1000

    Don't use these

--- web/index.php
+++ web/index.php
@@ -5,7 +5,5 @@ ini_set('display_errors', 0);
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = require 'boostrap.php';
-require __DIR__.'/../resources/config/prod.php';
-require __DIR__.'/../src/controllers.php';
 
 $app['http_cache']->run();
