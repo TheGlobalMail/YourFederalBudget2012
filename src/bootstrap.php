c8c1d5151d6edf89adc886c8eae53cb651b7fa8a
Andrew Cobby <andrew.cobby@theglobalmail.org>
2012-08-09 14:26:01 +1000

    Forgot to 'use' HttpFoundation\Response

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ParameterBag;
 
 $app = new Silex\Application();
