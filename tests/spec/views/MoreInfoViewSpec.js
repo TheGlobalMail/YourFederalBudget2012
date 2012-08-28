5e5725eb54625677a78d523645b63abeee2373bf
Andrew Cobby <andrew.cobby@theglobalmail.org>
2012-08-28 15:03:26 +1000

    Added read more popup

--- tests/spec/views/MoreInfoViewSpec.js
+++ tests/spec/views/MoreInfoViewSpec.js
@@ -1,13 +1,52 @@
 describe("More Info View", function() {
-    var moreInfoView, $el = $('<div><div class="title"></div><div class="info"></div></div>');
+    var moreInfoView, $el = $([
+        '<div>',
+            '<div class="title"></div>',
+            '<div class="info"></div>',
+            '<div class="modal extended-info hide fade">',
+                '<div class="modal-body"></div>',
+            '</div>',
+            '<a href="#" class="read-more">Re</a>',
+        '</div>'
+    ].join(""));
 
     beforeEach(function() {
         moreInfoView = new TGM.Views.MoreInfoView({ el: $el.clone() });
+        this.clock = sinon.useFakeTimers();
+        moreInfoView.$el.appendTo('body');
     });
 
-    it("should update info when active category changes", function() {
+    afterEach(function() {
+        moreInfoView.$el.remove();
+        this.clock.restore();
+    });
+
+    it("should update short info and title when active category changes", function() {
         TGM.vent.trigger('BudgetAllocatorCategory:expanding', 'health');
 
-        expect(moreInfoView.$title).toHaveText('Health');
+        expect(moreInfoView.$title).toHaveText(/Health/);
+        expect(moreInfoView.$info).toHaveText(/Health/);
+    });
+
+    describe("Extended Info", function() {
+        beforeEach(function() {
+            TGM.vent.trigger('BudgetAllocatorCategory:expanding', 'health');
+        });
+
+        it("shouldn't be visible by default", function() {
+            expect(moreInfoView.$extendedInfo).toHaveClass('hide');
+        });
+
+        it("should show the modal when read more is click", function() {
+            moreInfoView.$('.read-more').trigger('click');
+            this.clock.tick(200)
+
+            expect(moreInfoView.$extendedInfo).toBeVisible();
+        });
+
+        it("should update the extended info when active category changes", function() {
+            TGM.vent.trigger('BudgetAllocatorCategory:expanding', 'education');
+            expect(moreInfoView.$modalBody).toHaveText(/Education/);
+        });
     });
 });
--- web/js/views/MoreInfoView.js
+++ web/js/views/MoreInfoView.js
@@ -1,18 +1,32 @@
 TGM.Views.MoreInfoView = Backbone.View.extend({
 
+    events: {
+        'click .read-more': "readMore"
+    },
+
     initialize: function()
     {
         _.bindAll(this);
-        this.$title = this.$('.title');
-        this.$info  = this.$('.info');
+        this.$title        = this.$('.title');
+        this.$info         = this.$('.info');
+        this.$extendedInfo = this.$('.extended-info');
+        this.$modalBody    = this.$('.modal-body');
+
         TGM.vent.on('BudgetAllocatorCategory:expanding', this.showCategory);
     },
 
     showCategory: function(categoryId)
     {
-        var category = DATA.categories[categoryId];
-        this.$title.text(category.label);
-        this.$info.html(category.info);
+        this.categoryId = categoryId;
+        this.category = DATA.categories[categoryId];
+        this.$title.text(this.category.label);
+        this.$info.html(this.category.info.short);
+        this.$modalBody.html(this.category.info.extended);
+    },
+
+    readMore: function()
+    {
+        this.$extendedInfo.modal('toggle')
     }
 
 });
