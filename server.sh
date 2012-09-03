4616be7aec75e2c5d17213f1a2255bfaf26737da
Andrew Cobby <andrew.cobby@theglobalmail.org>
2012-09-03 12:15:26 +1000

    Modularisationing

--- server.sh
+++ server.sh
@@ -2,11 +2,19 @@
 
 PHP=`which php`
 
-if [ $1 == "dev" ]; then
+function devserver {
     $PHP -S 0.0.0.0:5000 -t web/ web/index_dev.php
-elif [ $1 == "test" ]; then
+}
+
+function testserver {
     $PHP -S 0.0.0.0:5001 -t ./ tests/index.php &
     pid=$!
+}
+
+function runtests {
+    testserver
     phantomjs tests/lib/phantom-jasmine/lib/run_jasmine_test.coffee http://localhost:5001/
     kill $pid
-fi
+}
+
+$1
