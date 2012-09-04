3323a43c1dff37e54395070691180600d57bb5cd
Andrew Cobby <andrew.cobby@theglobalmail.org>
2012-09-04 11:53:33 +1000

    Base model class

--- src/DGM/Models/Model.php
+++ src/DGM/Models/Model.php
@@ -2,9 +2,29 @@
 
 namespace DGM\Models;
 
+use DGM\Database\MongoDB;
+
 abstract class Model
 {
 
+    protected $db;
+    protected $createdAt;
 
+    public function __construct(MongoDB $db)
+    {
+        $this->db = $db;
+        $this->init();
+    }
+
+    public function save()
+    {
+        $coll = $this->db->getCollection($this->collection);
+        $data = $this->jsonSerialize();
+
+        $coll->insert($data);
+        $this->id = $data['_id'];
+    }
+
+    public function init() {}
 
 }
