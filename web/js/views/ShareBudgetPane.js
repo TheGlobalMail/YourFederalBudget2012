bfba3f11e230bd26e19d59135c0c326e732f599b
Andrew Cobby <andrew.cobby@theglobalmail.org>
2012-09-11 08:51:57 +1000

    Manually track URL and title for sharing

--- web/js/views/ShareBudgetPane.js
+++ web/js/views/ShareBudgetPane.js
@@ -12,6 +12,7 @@ TGM.Views.ShareBudgetPane = TGM.Views.SidePane.extend({
 
         this.$budgetName = this.$('.budget-name');
         this.$budgetUrl  = this.$('.budget-url');
+        this.$shareButtons = this.$('.share-buttons');
 
         this.clip = new ZeroClipboard.Client();
         this.clip.setHandCursor(true);
@@ -31,6 +32,8 @@ TGM.Views.ShareBudgetPane = TGM.Views.SidePane.extend({
         this.$budgetName.text(this.model.get('name'));
         this.$budgetUrl.val(this.model.getUrl());
         this.clip.setText(this.model.getUrl());
+        this.$shareButtons.attr('addthis:url', this.model.getUrl());
+        this.$shareButtons.attr('addthis:title', "Check out my budget");
     },
 
     onBudgetUrlFocus: function()
