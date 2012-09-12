34b6c4cae975ab3017c4a928190572031fcc4ff7
Andrew Cobby <andrew.cobby@theglobalmail.org>
2012-09-12 14:55:11 +1000

    Detect SVG support

--- web/js/bootstrap.js
+++ web/js/bootstrap.js
@@ -1,5 +1,13 @@
 window.location.origin = window.location.origin || window.location.protocol+'//'+window.location.host+'/';
 
+(function() {
+    var hasSVG = !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect;
+
+    if (!hasSVG) {
+        $('html').addClass('no-svg');
+    }
+})();
+
 var TGM = { Views: {}, Models: {}, Routers: {}, Collections: {} };
 TGM.vent = _.extend({}, Backbone.Events);
 TGM.vent.publish = TGM.vent.trigger;
