892beb7f2005f9b588b70e8caac764c8a50275d6
Andrew Cobby <andrew.cobby@gmail.com>
2012-09-26 10:58:00 +1000

    User can see category allocation amount under Budget Configurator slider for active category
    Animate bars with the same settings as the allocation bar
    
    Trigger a new event when the pretax income changes
    
    Add a slight delay to hiding the app overlay
    
    Show slider amount under slider (only when active)
    
    Add styling for slider amount
    
    Invoke collapse on the rest of the category allocator views
    
    Test category allocation views show exact amounts and respond to pretax income changes
    
    Fixed styling to show longer numbers in the exact amount

--- tests/spec/views/CategoryAllocationSpec.js
+++ tests/spec/views/CategoryAllocationSpec.js
@@ -31,15 +31,33 @@ describe("Category Allocation View", function() {
         this.clock.restore();
     });
 
-    it("should active when collapsed", function() {
+    it("should hide the slider amount when collapsed", function() {
         categoryAllocation.collapse();
         this.clock.tick(500);
 
         expect(categoryAllocation.$el).not.toHaveClass('active');
+        expect(categoryAllocation.$('.ui-slider-amount')).toBeHidden();
+    });
+
+    it("should show the slider amount when activated", function() {
+        categoryAllocation.expand();
+        this.clock.tick(500);
+
+        expect(categoryAllocation.$el).toHaveClass('active');
+        expect(categoryAllocation.$('.ui-slider-amount')).toBeVisible();
     });
 
     it("should update the slider when the model changes", function() {
         model.set('defense', 30);
         expect(categoryAllocation.$slider.slider('value')).toEqual(30);
+        expect(categoryAllocation.$sliderAmount).toHaveText('$30.0b');
+    });
+
+    it("should switch to income based amounts when pretax income is calculated", function() {
+        model.pretaxIncomeAmounts['defense'] = 213.34;
+        categoryAllocation.activeToggleName = 'your-pretax-income';
+        categoryAllocation.refreshAmount(model);
+
+        expect(categoryAllocation.$sliderAmount).toHaveText('$213.34');
     });
 });
