9e68fb19d32b85aafcbcc7559bafa58921dd23c3
Andrew Cobby <andrew.cobby@gmail.com>
2012-09-28 16:22:28 +1000

    Add test server command

--- server.sh
+++ server.sh
@@ -7,9 +7,14 @@ function devserver {
     $PHP -S 0.0.0.0:5000 -t web/ web/index_dev.php
 }
 
+function testserver {
+    $PHP -S 0.0.0.0:5050 -t ./ tests/index.php
+}
+
 function runtests {
     $PHP -S 0.0.0.0:5001 -t ./ tests/index.php &
     pid=$!
+    sleep 0.2
     phantomjs tests/lib/phantom-jasmine/lib/run_jasmine_test.coffee http://localhost:5001/
     success=$?
     kill $pid
