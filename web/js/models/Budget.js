3f5b5f22e16983b3478ff135c93c11497e70725c
Andrew Cobby <andrew.cobby@gmail.com>
2012-10-10 11:05:07 +1100

    Add Google Analytics and custom events

--- web/js/models/Budget.js
+++ web/js/models/Budget.js
@@ -227,6 +227,15 @@ TGM.Models.Budget = Backbone.Model.extend({
         }
 
         return $.post('/api/budget/flag-abuse/' + this.id);
+    },
+
+    sync: function(method)
+    {
+        if (method == "update" || "method" == "create") {
+            _gaq.push(['_trackEvent', 'Budget', _(method).capitalize(), this.get('name')])
+        }
+
+        return Backbone.Model.prototype.sync.apply(this, arguments);
     }
 
 });
